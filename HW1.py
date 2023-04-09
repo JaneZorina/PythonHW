88e039844f7b604fe347e5efefc8816e8075e0ac
unknown <gurovae@yahoo.com>
2023-04-09 21:18:14 +0300

    first HW

---
+++ HW1.py
@@ -0,0 +1,53 @@
+#Задача1
+
+# a=int(input())
+# if a>7 or a<1:
+#     print('Нет такого дня')
+# if a==1:
+#     print('Понедельник')
+# if a==2:
+#     print('Вторник')
+# if a==3:
+#     print('Среда')
+# if a==4:
+#     print('Четверг')
+# if a==5:
+#     print('Пятница')
+# if a==6:
+#     print('Суббота') 
+# if a==7:
+#     print('Воскресенье')
+
+#Задача 2
+
+# x1=int(input())
+# y1=int(input())
+# x2=int(input())
+# y2=int(input())
+# s=abs(sqrt(((x2-x1)**2)+((y2-y1)**2)))
+# print(s)
+
+#Задача 3
+# a=int(input())
+# if a<1 or a>4:
+#     print('Нет такой четверти')
+# if a==1:
+#     print('x>0,y>0')
+# if a==2:
+#     print('x<0,y>0')
+# if a==3:
+#     print('x<0,y<0')
+# if a==4:
+#     print('x>0,y<0')
+
+#Задача 4
+# n=int(input())
+# a=0
+# if n>0:
+#     for a in range(2,n+1,2):
+#         print(a)
+# if n<0:
+#     for a in range(-2,n-1,-2):
+#         print(a)
+# if n==0:
+#     print('Нет целых чисел')
