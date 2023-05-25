7c9170edbabfddd1ecbb95f3d707d729b0ba3ae5
unknown <gurovae@yahoo.com>
2023-05-25 15:23:43 +0300

    HW7

---
+++ HW7.py
@@ -0,0 +1,24 @@
+#Создайте пользовательский аналог метода map()
+# numbers=[2,3,4,5]
+# list=[]
+# for num in numbers:
+#     list.append(num**2)
+# print(list)
+    
+#Создайте декоратор, повторяющий функцию заданное количество раз.
+
+# def repeat(fn):
+#     def wrapper(*args, **kwargs):
+#         for _ in range(5):
+#             result = fn(*args, **kwargs)
+#         return result
+
+#     return wrapper 
+
+# @repeat
+# def greetings():
+#     print("Привет")
+
+# greetings()
+
+
